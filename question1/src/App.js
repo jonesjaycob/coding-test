2d4e0d6ddb86d71a7f8b39f9f7cf1794222573e8
Jones <JJ86901@jj86901s-air.lan>
2024-03-28 07:15:43 -0700

    answer

--- question1/src/App.js
+++ question1/src/App.js
@@ -1,7 +1,21 @@
 import "./App.css";
-
+import { useState } from "react";
+import { entries } from "./library";
 function App() {
-  return <div className="App">{/* Insert your code here */}</div>;
+  const [value, setValue] = useState("");
+  return (
+    <div className="App">
+      <input type="text" onChange={(e) => setValue(e.target.value)} />
+      <ul>
+        {value !== "" &&
+          entries
+            .filter((entry) =>
+              entry.title.toLowerCase().includes(value.toLowerCase())
+            )
+            .map((x) => <li key={x.id}>{x.title}</li>)}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
